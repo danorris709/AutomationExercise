7102f29b079449f52848c402d4474275f1b70187
Daniel Norris <dnorris443@gmail.com>
2026-07-20 13:00:20 +0100

    chore(test): add initial test project files

---
+++ src/test/java/com/automationexercise/AutomationExerciseTestSuite.java
@@ -0,0 +1,22 @@
+package com.automationexercise;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("/features")
+@ConfigurationParameter(
+        key = GLUE_PROPERTY_NAME,
+        value = "com.automationexercise"
+)
+@ConfigurationParameter(
+        key = PLUGIN_PROPERTY_NAME,
+        value = "net.serenitybdd.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:build/test-results/timeline"
+)
+public class AutomationExerciseTestSuite {
+}
---
+++ src/test/java/com/automationexercise/page/HomePage.java
@@ -0,0 +1,9 @@
+package com.automationexercise.page;
+
+import net.serenitybdd.annotations.DefaultUrl;
+import net.serenitybdd.core.pages.PageObject;
+
+@DefaultUrl("https://automationexercise.com/")
+public class HomePage extends PageObject {
+
+}
