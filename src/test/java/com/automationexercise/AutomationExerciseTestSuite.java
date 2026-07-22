06337e5938136a0388c381b7216542d3e790b5a1
Daniel Norris <dnorris443@gmail.com>
2026-07-22 16:53:16 +0100

    feat(api): add session local storage for account context

--- src/test/java/com/automationexercise/AutomationExerciseTestSuite.java
+++ src/test/java/com/automationexercise/AutomationExerciseTestSuite.java
@@ -1,7 +1,9 @@
 package com.automationexercise;
 
+import com.automationexercise.api.ScenarioAccountContext;
 import io.restassured.RestAssured;
 import io.restassured.parsing.Parser;
+import net.serenitybdd.core.Serenity;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
@@ -22,6 +24,16 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 )
 public class AutomationExerciseTestSuite {
 
-    public static final String CREATED_USER = "createdUserId";
+    public static final String ACCOUNT_CONTEXT = "accountContext";
 
+    public static ScenarioAccountContext getAccountContext() {
+        ScenarioAccountContext context = Serenity.sessionVariableCalled(ACCOUNT_CONTEXT);
+
+        if (context == null) {
+            context = new ScenarioAccountContext();
+            Serenity.setSessionVariable(ACCOUNT_CONTEXT).to(context);
+        }
+
+        return context;
+    }
 }
